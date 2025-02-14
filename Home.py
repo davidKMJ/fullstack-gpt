e99a8e79b8a61f3148efcc7c8b4d3ea1183831e1
David Minjae Ko <onedriveonly11@gmail.com>
2025-02-14 20:06:37 +0900

    Learned about RAG and Streamlit

---
+++ Home.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Welcome to fullstack-gpt",
+    page_icon="🤖",
+)
+st.title("Home")
+
+st.markdown(
+"""
+# Welcome to fullstack-gpt portfolio!
+
+Here are the apps:
+
+- [ ] [Document GPT](/DocumentGPT)
+- [ ] [Private GPT](/PrivateGPT)
+
+"""
+)
